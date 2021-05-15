1e86dbc89c369d14b15dd90f5ce961971a9af0e7
aldihsuhandi <aldihsuhandi697@gmail.com>
2021-05-15 07:39:53 +0700

    updating basic app structure

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,50 @@ void main() => runApp(MaterialApp(
     home: Home(),
 ));
 
-
 class Home extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-        appBar: AppBar(
-            title: Text('Homepage'),
-            centerTitle: true,
-            backgroundColor: Colors.black87,
-        ),
-        body: Center(
-            child: Text(
-                "Test text",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5,
-                    color: Colors.grey[600],
-                    fontFamily: 'Roboto',
+            appBar: AppBar(
+                // leading: IconButton(
+                //     onPressed: null,
+                //     icon: Icon(Icons.menu),
+                //     tooltip: "Navigation",
+                // ),
+                title: Text("Homapage"),
+            ),
+            body: Center(
+                child: Text(
+                    "Test text",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                        color: Colors.grey[600],
+                        fontFamily: 'Roboto',
+                    ),
                 ),
             ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Text("click"),
-            backgroundColor: Colors.black87,
-        ),
-    );
+            drawer: NavMenu(),
+            floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                child: Text("click"),
+                backgroundColor: Colors.black87,
+            ),
+        );
+    }
+}
+
+class NavMenu extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Drawer(
+            child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget> [
+                    ListTile(title: Text("Test 1"), onTap: () {Navigator.pop(context);},),
+                ]
+            ),
+        );
     }
 }
